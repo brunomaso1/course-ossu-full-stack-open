a93b503d0da733a14d6b6653c154d81050ef9ed3
Bruno Masoller <brunomaso1@hotmail.com>
2023-09-19 19:21:08 -0300

    2.12: The Phonebook step7

--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -1,4 +1,5 @@
 import { React } from "react";
+import axios from 'axios';
 
 export function PersonForm({ persons, setPersons, newName, setNewName, newNumber, setNewNumber }) {
 
@@ -7,13 +8,26 @@ export function PersonForm({ persons, setPersons, newName, setNewName, newNumber
         if (persons.some((person) => person.name.toLowerCase() === newName.toLowerCase())) {
             alert(`${newName} is already added to the phonebook`);
         } else {
-            setPersons(persons.concat({
+            const newPerson = {
                 name: newName,
                 number: newNumber,
                 id: persons.length + 1
-            }));
-            setNewNumber('');
-            setNewName('');
+            }
+
+            const url = 'http://localhost:3001/persons'
+
+            axios.post(url, newPerson)
+                .then((response) => {
+                    setPersons(persons.concat(response.data));
+                })
+                .catch((response) => {
+                    alert(`Person ${newPerson.name} can not be saved`)
+                    console.log(response);
+                })
+                .finally(() => {
+                    setNewNumber('');
+                    setNewName('');
+                })
         }
     }
 
