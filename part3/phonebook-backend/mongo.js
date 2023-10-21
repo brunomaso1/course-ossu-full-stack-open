269f78ac31ed7ab48547d105767ce71aeecda56a
Bruno Masoller <brunomaso1@hotmail.com>
2023-10-21 08:55:21 -0300

    3.12: Command-line database

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+// Handle errors...
+function handleError(message) {
+    console.log(message);
+    process.exit(1);
+}
+
+if (process.argv.length < 3) { handleError('password missing') }
+if (process.argv.length > 5) { handleError('too many parameters') }
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://test:${password}@cluster0.02cwenu.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    name: name,
+    number: number,
+})
+
+if (name || number) {
+    person.save().then(result => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    console.log("phonebook:");
+    Person.find({}).then(response => {
+        response.forEach(person => {
+            console.log(`${person.name} ${person.number}`);
+        });
+        mongoose.connection.close();
+    })
+}
