6265fe8a93ec0d3e8c4893c18f5c1f896b354d2b
Bruno Masoller <brunomaso1@hotmail.com>
2023-10-21 12:14:44 -0300

    3.13: Phonebook database, step1

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const Person = require('./models/person')
 
 morgan.token('body', req => JSON.stringify(req.body))
 
@@ -10,32 +12,7 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"
-    }
-]
-
-app.get('/', (request, response) => {
-    return response.send('<h1>Server is up</h1>');
-})
+let persons = []
 
 app.get('/info', (request, response) => {
     let htmlResp = `<p>Phonebook has info for ${persons.length} people</p>`;
@@ -46,7 +23,7 @@ app.get('/info', (request, response) => {
 
 app.route('/api/persons')
     .get((request, response) => {
-        return response.json(persons);
+        Person.find({}).then(persons => response.json(persons))
     })
     .post((request, response) => {
         const body = request.body;
