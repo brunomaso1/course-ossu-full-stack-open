717c499d7eedcfedbe921a9b2820d3efa30c9130
Bruno Masoller <brunomaso1@hotmail.com>
2023-10-26 19:21:03 -0300

    4.4: helper functions and unit tests, step2

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -2,6 +2,10 @@ const dummy = (blogs) => {
     return 1
 }
 
-module.exports = {
-    dummy
+const totalLikes = (blogs) => {
+    return blogs.reduce((previousValue, currentValue) => {
+        return previousValue + currentValue.likes
+    }, 0)
 }
+
+module.exports = { dummy, totalLikes }
