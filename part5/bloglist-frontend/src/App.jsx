fcc1f0ddaf12d702fee381e209712864694bc058
Bruno Masoller <brunomaso1@hotmail.com>
2023-11-14 17:34:00 -0300

    5.1: bloglist frontend, step1

---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
+import blogService from './services/blogs'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs(blogs)
+    )
+  }, [])
+
+  if (!user) return <LoginForm setUser={setUser}></LoginForm>
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      <p>{user.name} logged in</p>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ part5/bloglist-frontend/src/components/LoginForm.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+import loginService from "../services/login"
+
+const LoginForm = ({ setUser }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function handleLogin(event) {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({ username, password })
+
+      localStorage.setItem('sessionToken', JSON.stringify(user.token))
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (error) {
+      console.log("Wrrong credentials: ", error);
+    }
+  }
+
+  return (
+    <>
+      <h1>log in to application</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          username: 
+          <input type="text" value={username} name="Username" onChange={({target}) => setUsername(target.value)} />
+        </div>
+        <div>
+          password: 
+          <input type="password" value={password} name="Password" onChange={({target}) => setPassword(target.value)} />
+        </div>
+        <div>
+          <button type="submit">login</button>
+        </div>
+      </form>
+    </>
+  )
+}
+
+export default LoginForm
