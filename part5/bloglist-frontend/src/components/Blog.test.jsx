1c832d7427e4e1c951c6361605f757d3e189dbfd
Bruno Masoller <brunomaso1@hotmail.com>
2024-02-21 22:57:07 -0300

    5.13: Blog List Test, step 2

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,11 +1,11 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { queryByRole, render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 const blog = {
   title: 'title1',
-  author: 'author1',
+  author: 'author1 url1',
   url: 'url1',
   likes: 0,
   user: {
@@ -13,7 +13,6 @@ const blog = {
     name: 'test1',
   }
 }
-
 
 describe('<Blog /> ', () => {
   test('Renders the blog title and author', () => {
@@ -28,6 +27,20 @@ describe('<Blog /> ', () => {
   test('Does not render the URL or number of likes by default', () => {
     render(<Blog blog={blog} setMustUpdateBlogs={null} user='test1' />)
 
-    screen.queryByText('url1', { exact: false }) === null && screen.queryByText('likes', { exact: false }) === null
+    !screen.queryByText('url1', { exact: false }) && !screen.queryByText('likes', { exact: false })
+  })
+
+  test('The button view is showed by default', () => {
+    render(<Blog blog={blog} setMustUpdateBlogs={null} user='test1' />)
+
+    screen.getByRole('button', { name: 'view' })
+  })
+
+  test.skip('Shown URL and number of likes when show button is clicked', async () => {
+    render(<Blog blog={blog} setMustUpdateBlogs={null} user='test1' />)
+    const user = userEvent.setup()
+
+    const button = screen.queryByRole('button', { name: 'view' })
+    await user.click(button)
   })
 })
---
+++ part5/bloglist-frontend/src/components/ToggableButton.test.jsx
@@ -0,0 +1,7 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+describe('<ToggableButton /> ', () => {
+  test.todo('Component is created')
+})
